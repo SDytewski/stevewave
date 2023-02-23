a391b1f30d9d482c688bbde308b662d6bc28ae6d
Steve Dytewski <dy2ski@gmail.com>
2023-02-22 20:58:15 -0800

    styled form button and moved contact form to center

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -85,27 +85,36 @@ export const Contact = () => {
             </Typography>        
         </Grid>  
 
+        <Grid item  xs={12} align="center">  
 
         <div className="container">
-<h1>FormSubmit Demo</h1>
-<form target="_blank" action="https://formsubmit.co/dy2ski@gmail.com" method="POST">
-    <div className="form-group">
-        <div className="form-row">
+        <h1>FormSubmit Demo</h1>
+        <form target="_blank" action="https://formsubmit.co/dy2ski@gmail.com" method="POST">
+            <div className="form-group">
+                <div className="form-row">
             <div className="col">
                 <input type="text" name="name" className="form-control" placeholder="Full Name" required />
             </div>
             <div className="col">
                 <input type="email" name="email" className="form-control" placeholder="Email Address" required />
             </div>
-        </div>
-    </div>
+                </div>
+            </div>
     <div className="form-group">
         <textarea placeholder="Your Message" className="form-control" name="message" rows="10" required></textarea>
     </div>
-    <button type="submit" className="btn btn-lg btn-dark btn-block">Submit Form</button>
+    <Grid item xs={12} align="center" sx={{mt: 0}}>  
+<Button type="submit" variant="contained" color="primary" sx={{ m: 2 }} xs={12}>Submit Form</Button>
+</Grid> 
+    
+   
 </form>
+
+
+ 
+
 </div>
-)
+</Grid> 
      
         </Grid>
         </div>
