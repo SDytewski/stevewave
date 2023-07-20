f6707a659d23bc89411005feb5d6672b358ee011
Steve Dytewski <dy2ski@gmail.com>
2023-07-19 19:10:44 -0700

    rendering spotify data on page in card

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -17,149 +17,150 @@ import { useEffect, useState } from "react";
 import backgroundmic from '../images/microphone.jpg';
 
 const classes = {
-    root: {
-      flexGrow: 1
-    },
+  root: {
+    flexGrow: 1
+  },
 }
 
 
 
-
-
-
-
-
 const styles = {
-    heroContainer: {
-      height: '90vh',
-    
-      backgroundImage: `url(${backgroundmic})`,
-     
-      backgroundSize: 'cover',
-    
+  heroContainer: {
+    height: '90vh',
+
+    backgroundImage: `url(${backgroundmic})`,
+
+    backgroundSize: 'cover',
+
+  },
+
+
+};
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff8f00" // This is an orange looking color
     },
+    secondary: {
+      main: "#ffcc80" //Another orange-ish color
+    },
+  }
 
-
-   };
-
-
-   const theme = createTheme({
-    palette: {
-       primary: {
-          main: "#ff8f00" // This is an orange looking color
-                 },
-       secondary: {
-          main: "#ffcc80" //Another orange-ish color
-                  },
-                }
-         
 });
 
 const CLIENT_ID = "78a30fe647724ab897180931bb58e282";
 const CLIENT_SECRET = "33b2264d59f042ad84024042692599cf"
 
 export const Songs = () => {
-//function expression is when a function is stored in a variable
+  //function expression is when a function is stored in a variable
 
-    const [roll, setRoll] = useState(false);
-    const  [accessToken, setAccessToken] = useState("");
+  // const [roll, setRoll] = useState(false);
+  const [spotify, setSpotify] = useState("");
+  const [accessToken, setAccessToken] = useState("");
 
-    // useEffect(() => {
-    //   setTimeout(() => {
-    //     // animation
-    //     setRoll(true);
-    //   }, 500);
-    // }, []);
- 
-    useEffect(() =>  {
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     // animation
+  //     setRoll(true);
+  //   }, 500);
+  // }, []);
 
-      const authParameters = {
-        method: 'POST',
+  useEffect(() => {
+
+    const authParameters = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body:
+        "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET
+
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => {
+        setAccessToken(data.access_token)
+        return data
+      })
+      .then(response => fetch("https://api.spotify.com/v1/artists/3FcIgTRVZZwASnXLHJ1JNr", {
+        method: 'GET',
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded" 
-        },
-        body: 
-          "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET
-    
-      }
-        fetch('https://accounts.spotify.com/api/token', authParameters)
-          .then(result => result.json())
-          .then(data => {
-            setAccessToken(data.access_token)
-            return data
-          })
-          .then(response => fetch("https://api.spotify.com/v1/artists/3FcIgTRVZZwASnXLHJ1JNr", {
-            method: 'GET',
-            headers: { 
-              "Authorization" : `Bearer ${response.access_token}`
-            }
-          }))    
-          .then(res => res.json())
-          .then(data1 => console.log(data1))      
-    
-    }, [])
+          "Authorization": `Bearer ${response.access_token}`
+        }
+      }))
+      .then(res => res.json())
+      .then(data1 => { setSpotify(data1); console.log(data1) })
 
-    return (
-        
+  }, [])
 
-        
+  return (
+
+
+
     <div style={classes.root}>
-       <Grid
-            container
-            spacing={0}
-            // direction="column"
-            // alignItems="center"
-            // justifyContent="center"
-            style={styles.heroContainer} 
+      <Grid
+        container
+        spacing={0}
+        // direction="column"
+        // alignItems="center"
+        // justifyContent="center"
+        style={styles.heroContainer}
 
-          >
-     
-       
-        <Grid item  xs={12}>      
-           
-           <Typography sx={{ color: "#A4A9AD", mt: 4, pt: 10 }} variant="h6" align="center">
-              <div className="glow">
-               SONGS
-                </div>
-            </Typography>  
-            <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Spotify Data
-        </Typography>
-        <Typography variant="h5" component="div">
-          
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          Picture
-        </Typography>
-        <Typography variant="body2">
-         something
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      
-    </Card>
-                  
-        </Grid>  
-            <Grid item xs={12} align="center">
-            <iframe src="https://open.spotify.com/embed/album/2IoHLWW6TmUp1QoJxyr7Gw?utm_source=generator"   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      >
 
-            {/* <iframe src="https://bandcamp.com/EmbeddedPlayer/track=3580342141/size=small/bgcol=ffffff/linkcol=f171a2/transparent=true/" seamless><a href="https://stevewave.bandcamp.com/track/babe-blade">Babe Blade by Steve Wave</a></iframe> */}
-            </Grid>
 
-            <Grid item xs={12} align="center">
+        <Grid item xs={12}>
 
-            <iframe src="https://open.spotify.com/embed/album/2e0Pcn6CSDmN7ss0YamLX7?utm_source=generator"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-           
-            </Grid>
-           
-            <Grid item xs={12} align="center">
-            <iframe src="https://open.spotify.com/embed/track/2hxhhr8YscoLlOfnd0Fepp?utm_source=generator"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </Grid>
+          <Typography sx={{ color: "#A4A9AD", mt: 4, pt: 10 }} variant="h6" align="center">
+            <div className="glow">
+              SONGS
+            </div>
+          </Typography>
+          <Box  display="flex"
+      justifyContent="center" sx={{ width: 1/4 }} > 
+          <Card sx={{ minWidth: 275 }}>
+
+            <CardContent style={{ backgroundColor: "grey" }}>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Spotify Data
+              </Typography>
+              <Typography variant="h5" component="div">
+
+              </Typography>
+              <div>
+                <ul>Name: {spotify.name}</ul>
+                <ul>Followers: {spotify.followers.total}</ul>
+                <ul>Link: {spotify.href}</ul>
+
+
+
+
+              </div>
+            
+            </CardContent>
+
+          </Card>
+          </Box>          
         </Grid>
-        </div>
-     
-    );
+        <Grid item xs={12} align="center">
+          <iframe src="https://open.spotify.com/embed/album/2IoHLWW6TmUp1QoJxyr7Gw?utm_source=generator" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+          {/* <iframe src="https://bandcamp.com/EmbeddedPlayer/track=3580342141/size=small/bgcol=ffffff/linkcol=f171a2/transparent=true/" seamless><a href="https://stevewave.bandcamp.com/track/babe-blade">Babe Blade by Steve Wave</a></iframe> */}
+        </Grid>
+
+        <Grid item xs={12} align="center">
+
+          <iframe src="https://open.spotify.com/embed/album/2e0Pcn6CSDmN7ss0YamLX7?utm_source=generator" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+
+        </Grid>
+
+        <Grid item xs={12} align="center">
+          <iframe src="https://open.spotify.com/embed/track/2hxhhr8YscoLlOfnd0Fepp?utm_source=generator" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </Grid>
+      </Grid>
+    </div>
+
+  );
 };
